939bef89b11aed8a623922536ef0e28817ba55da
Shenz12 <shenil8137@gmail.com>
2024-01-18 22:29:57 -0800

    app
    app

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+import cv2
+from tensorflow.keres.models import load_model
+global vs
+from detect_mask_video import project
+
 
 app= Flask(__name__)
 
+prototxtPath="face_detector/deploy.prototxt"
+weightsPath="face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+faceNet=cv2.dnn.readNet(prototxtPath,weightsPath)
+maskNet=load_model("mask_detector.model")
+
 
 @app.route('/')
-def name():
-    return 'Favas'
+def home():
+    return render_template("index.html")
+
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
+@app.route('/mask')
+def emotion():
+    project()
+    return redirect(url_for('main'))
+
 
 
 # main driver function
@@ -13,4 +33,5 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(debug=True)
+
